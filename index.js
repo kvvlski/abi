50e0ea8d6e600ca66c1f2d2c8e2c66e432d863f4
kvvlski <enthropy0@gmail.com>
2025-05-03 01:06:56 +0400

    refactor: replace ERC20 and WETH exports with JSON files

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 Object.defineProperty(exports, "ERC20", {
   enumerable: true,
-  get: () => require("./abi/erc20").ERC20,
+  get: () => require("./abi/erc20.json"),
 });
 
 Object.defineProperty(exports, "WETH", {
   enumerable: true,
-  get: () => require("./abi/weth").WETH,
+  get: () => require("./abi/weth.json"),
 });
