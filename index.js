a4e98979e67e5a50b82f081ff77c7fd957e0c978
kvvlski <enthropy0@gmail.com>
2025-05-03 01:04:29 +0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+
+Object.defineProperty(exports, "ERC20", {
+  enumerable: true,
+  get: () => require("./abi/erc20").ERC20,
+});
+
+Object.defineProperty(exports, "WETH", {
+  enumerable: true,
+  get: () => require("./abi/weth").WETH,
+});
